8efd7bcab7ec44ac4957af0df7c513c27f2de171
KseniiaNedilko <kseniasvitova@gmail.com>
2018-03-13 22:04:51 +0000

    Initial

--- js/smooth-scroll.js
+++ js/smooth-scroll.js
@@ -9,3 +9,19 @@ $(function() {
         }
     });
 });
+
+$('.carousel').carousel({
+  interval: 3000
+})
+
+new WOW().init();
+
+$(window).scroll(function() {
+    $('.mov').each(function(){
+        var imagePos = $(this).offset().top;
+        var topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow+200) {
+            $(this).addClass('slideInRight');
+        }
+    });
+});
